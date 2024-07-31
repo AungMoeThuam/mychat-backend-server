8bf6725b4b11f44095c71a653a86d820acda6614
AUNG MOE THU <aungmoethu.2020.mc@gmail.com>
2024-08-01 07:15:38 +0800

    update database connection configuration

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const url = "mongodb://aungmoethu:my%40chat%40aung@127.0.0.1:27017/mychatapp";
-// const url = "mongodb://localhost:27017/mychatapp";
+const url = `mongodb://127.0.0.1:27017/${process.env.DB_NAME}`;
 
 async function db() {
   try {
-    await mongoose.connect(url);
-    console.log("database connection is running ...! ");
+    await mongoose.connect(url, {
+      user: process.env.DB_USERNAME,
+      pass: process.env.DB_PASSWORD,
+    });
+    console.log("database connection is running successfully ...! ");
   } catch (error) {
     console.error(error);
   }
